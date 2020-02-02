4a3dd850837e5b8974653e4f6b4d1d484e5182f7
Matt <matt.heslington@icloud.com>
2020-02-02 15:10:12 +0800

    Reworded and new pictures added.

--- config.js
+++ config.js
@@ -1,37 +1,32 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
-  manifestName: 'Eventually',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
-  // social
-  heading: 'Eventually',
-  subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
-  socialLinks: [
-    {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
-    },
-    {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
-    },
-    {
-      icon: 'fa-envelope-o',
-      name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
-    },
-  ],
+    siteTitle: 'Ultimate Addresses | Premium Real Estate Domain', // <title>
+    manifestName: 'Eventually',
+    manifestShortName: 'Landing', // max 12 characters
+    manifestStartUrl: '/',
+    manifestBackgroundColor: '#663399',
+    manifestThemeColor: '#663399',
+    manifestDisplay: 'standalone',
+    manifestIcon: 'src/assets/img/website-icon.png',
+    pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+    // social
+    heading: 'UltimateAddresses.com',
+    subHeading:
+        "A premium domain name for a premium real estate portfolio website. Complete with Instagram and Facebook branded accounts.",
+    socialLinks: [
+        {
+            icon: 'fa-instagram',
+            name: 'Instagram',
+            url: 'https://www.instagram.com/ultimateaddresses/',
+        },
+        {
+            icon: 'fa-facebook',
+            name: 'Facebook',
+            url: 'https://www.facebook.com/ultimateaddresses/',
+        },
+        {
+            icon: 'fa-envelope-o',
+            name: 'Email',
+            url: 'mailto:matt@ultimateaddresses.com',
+        },
+    ],
 };
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
 
 export class EmailForm extends Component {
-  constructor() {
-    super();
-    this.state = { message: '' };
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = { message: '' };
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
-  onSubmit(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.setState({ message: 'Thank you!' });
-    setTimeout(() => {
-      this.setState({ message: '' });
-    }, 3000);
-  }
+    onSubmit(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({ message: 'Thank you!' });
+        setTimeout(() => {
+            this.setState({ message: '' });
+        }, 3000);
+    }
 
-  render() {
-    const { message } = this.state;
-    return (
-      <form id="signup-form" onSubmit={this.onSubmit} method="post" action="#">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email Address"
-        />
-        <input type="submit" value="Sign Up" />
-        <span className={`${message ? 'visible success' : ''} message`}>
-          {message}
-        </span>
-      </form>
-    );
-  }
+    render() {
+        const { message } = this.state;
+        return (
+            <form id="signup-form" onSubmit={this.onSubmit} method="post" action="#">
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Sample Signup Form"
+                />
+                <input type="submit" value="Tell Me More" />
+                <span className={`${message ? 'visible success' : ''} message`}>
+                    {message}
+                </span>
+            </form>
+        );
+    }
 }
 
 export default EmailForm;
